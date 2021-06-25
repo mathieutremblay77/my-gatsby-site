449c2740dc7a973eec0214ed89e63d8d23f5aca1
Julie <jfortin@pigecommunication.com>
2021-06-25 15:02:46 -0400

    gatsby tutorial pt. 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>Je suis en train d'apprendre Gatsby</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
